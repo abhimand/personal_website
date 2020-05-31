7b08ee7fa62b767661ac0cb68cf53d5cfa490bb3
abhimand <abhi.mand@utexas.edu>
2020-05-31 16:43:27 -0500

    Updates

---
+++ precache-manifest.1324172b2c1d51f8d247fe9d9d3d47a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f3a0367c19f499f53fdb3517eafc1b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3371d649081361333fdd",
+    "url": "/my-app/static/css/main.f78f7e94.chunk.css"
+  },
+  {
+    "revision": "6c294e2d52253a5113e6",
+    "url": "/my-app/static/js/2.1d38916e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.1d38916e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3371d649081361333fdd",
+    "url": "/my-app/static/js/main.7863e12d.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  },
+  {
+    "revision": "e5a15e6c51a2ba8893745ae89892343d",
+    "url": "/my-app/static/media/backgroundImage.e5a15e6c.jpg"
+  },
+  {
+    "revision": "4d934907a11b3bd7e0ba7696bf9ff3b1",
+    "url": "/my-app/static/media/fujitsu.4d934907.jpg"
+  },
+  {
+    "revision": "da6cca87c4089328db4a9fd01dde838a",
+    "url": "/my-app/static/media/profPic.da6cca87.JPG"
+  }
+]);
